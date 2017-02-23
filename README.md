563e293e5b61540304ce48ca0011c6205669f408
Lucas Ferreira <lukin_fs@hotmail.com>
2017-02-23 16:38:24 -0300

    Adicionando modulo socket.io

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 # Instalar consign -> npm install consign --save 
   body-parser - npm install body-parser --save
   express validator - npm install express-validator --save
+  socket.io - npm install socket.io --save
--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -4,6 +4,8 @@ var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
 
-app.listen(82, function(){
+var server = app.listen(82, function(){
 	console.log('Servidor online');
 });
+
+require('socket.io').listen(server); // desta forma, requisições para a porta 82 são recebidas requisições http tanto , websockets
