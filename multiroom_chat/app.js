4a090c1889ac1e94ec2397c11be04e3cc1b1b5e0
Lucas Ferreira <lukin_fs@hotmail.com>
2017-02-24 16:52:35 -0300

    Exibir mensagem de novos usuarios ao entrar no chat

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -8,4 +8,15 @@ var server = app.listen(82, function(){
 	console.log('Servidor online');
 });
 
-require('socket.io').listen(server); // desta forma, requisições para a porta 82 são recebidas requisições http tanto , websockets
+var io = require('socket.io').listen(server); // desta forma, requisições para a porta 82 são recebidas requisições http tanto , websockets
+
+app.set('io',io);
+
+/* criar a conexao par ao websocket */
+io.on('connection', function(socket){
+	console.log('Usuario conectou');
+	
+	socket.on('disconnect',function(){
+		console.log('usuario desconectou');
+	})
+});
