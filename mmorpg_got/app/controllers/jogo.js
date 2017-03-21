b3a1da30a8917f63dae80c9789f6b789428b14cf
Lucas Ferreira <lukin_fs@hotmail.com>
2017-03-21 11:11:56 -0300

    Exibir views de suditos e pergaminho com Ajax

--- mmorpg_got/app/controllers/jogo.js
+++ mmorpg_got/app/controllers/jogo.js
@@ -22,3 +22,17 @@ module.exports.sair = function(application, req, res){
 	});
 
 }
+
+module.exports.suditos = function(application, req, res){
+	
+	res.render("aldeoes", {validacao : {}} );
+	
+
+}
+
+module.exports.pergaminhos = function(application, req, res){
+	
+	res.render("pergaminhos", {validacao : {}} );
+	
+
+}
