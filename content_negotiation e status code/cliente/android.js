7736d6cc23faa236854156f6d634b978b2be91f1
Lucas Ferreira <lukin_fs@hotmail.com>
2017-03-29 15:28:04 -0300

    Accept e Content-type, envio e recebimento de conteúdos

---
+++ content_negotiation e status code/cliente/android.js
@@ -0,0 +1,37 @@
+var http = require('http');
+
+var opcoes = {
+	hostname: 'localhost',
+	port : 86,
+	path : '/',
+	method: 'post',
+	headers : {
+		'Accept' : 'application/json',
+		'Content-type' : 'application/json '
+	}
+}
+
+// Content-type 
+
+var html = 'nome=Lucas' // x-www-form-urlencoded
+var json = {nome: 'Lucas'};
+var string_json = JSON.stringify(json);
+
+var buffer_corpo_response = [];
+
+var req = http.request(opcoes, function(res){
+	
+	res.on('data', function(pedaco){
+		buffer_corpo_response.push(pedaco);
+	});
+
+	res.on('end', function(){
+		var corpo_response = Buffer.concat(buffer_corpo_response).toString();
+		console.log(corpo_response);
+	});
+
+});
+
+req.write(string_json);
+
+req.end();
