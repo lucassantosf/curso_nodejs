5dd6f48e54761b1eb737ec022be477a4ad58af55
Lucas Ferreira <lukin_fs@hotmail.com>
2017-04-01 23:35:34 -0300

    Método Post - incluir docs no mongoDB

--- instagram_clone/api/server.js
+++ instagram_clone/api/server.js
@@ -12,9 +12,33 @@ var port = 8080;
 
 app.listen(port);
 
+var db = new mongodb.Db(
+	'instagram',
+	new mongodb.Server('localhost', 27017, {}),
+	{}
+);
+
+
 console.log('Servidor HTTP esta escutando na porta '+ port);
 
 app.get('/', function(req, res){
 	
 	res.send({msg : 'Olá'});
+});
+
+app.post('/api',function(req,res){
+	
+	var dados = req.body;
+	db.open( function(err, mongoclient){
+		mongoclient.collection('postagens', function(err, collection){
+			collection.insert(dados, function(err, records){
+				if(err){
+					res.json(err);
+				}else{
+					res.json(records);
+				}
+				mongoclient.close();
+			});
+		});
+	});	
 });
